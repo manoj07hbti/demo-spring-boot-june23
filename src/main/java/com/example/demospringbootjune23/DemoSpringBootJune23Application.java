16307878b534e805f1c7a5101ee3013303cac18c
manoj07hbit <60manojkumar@gmail.com>
2023-10-31 18:21:01 +0530

    demo commit

---
+++ src/main/java/com/example/demospringbootjune23/DemoSpringBootJune23Application.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootjune23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootJune23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootJune23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demospringbootjune23/DemoSpringBootJune23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootjune23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootJune23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
