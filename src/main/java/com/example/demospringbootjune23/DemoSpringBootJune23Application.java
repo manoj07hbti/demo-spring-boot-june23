930703c7ef3f8cf36268a2366ee99b0776b66ee3
manoj07hbti <60manojkumar@gmail.com>
2023-11-09 19:53:53 +0530

    empList

--- src/main/java/com/example/demospringbootjune23/DemoSpringBootJune23Application.java
+++ src/main/java/com/example/demospringbootjune23/DemoSpringBootJune23Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoSpringBootJune23Application {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootJune23Application.class, args);
 	}
