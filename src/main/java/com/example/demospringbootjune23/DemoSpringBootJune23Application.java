361e53c30d3b1fd6e515e28a38679d43b3f457b8
manoj07hbti <60manojkumar@gmail.com>
2023-11-09 17:04:22 +0530

    test

--- src/main/java/com/example/demospringbootjune23/DemoSpringBootJune23Application.java
+++ src/main/java/com/example/demospringbootjune23/DemoSpringBootJune23Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoSpringBootJune23Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootJune23Application.class, args);
 	}
