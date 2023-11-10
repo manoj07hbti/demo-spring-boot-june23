01a147c7aed2fea35232196988ed79737a43c8ef
manoj07hbti <60manojkumar@gmail.com>
2023-11-10 20:11:23 +0530

    Postman

--- src/main/java/com/example/demospringbootjune23/controller/EmpController.java
+++ src/main/java/com/example/demospringbootjune23/controller/EmpController.java
@@ -2,10 +2,7 @@ package com.example.demospringbootjune23.controller;
 
 import com.example.demospringbootjune23.model.Employee;
 import com.example.demospringbootjune23.model.Student;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -14,21 +11,28 @@ public class EmpController {
 
     ArrayList<Employee> employeeArrayList=new ArrayList<>();
 
+
+    @PostMapping("/add_emp_obj")
+    public String addEmployee(@RequestBody Employee employee){
+        employeeArrayList.add(employee);
+
+        return "Employee added...";
+    }
     //Add
-    @RequestMapping("/add_emp/{name}/{id}/{dept}")
+    @PostMapping("/add_emp/{name}/{id}/{dept}")
     public String addEmp(@PathVariable String name, @PathVariable int id,@PathVariable String dept){
         Employee employee=new Employee(name,id,dept);
        employeeArrayList.add(employee);
      return "Employee Added Successfully...";
     }
 
-    @RequestMapping("/get_all_emp")
+    @GetMapping("/get_all_emp")
     public ArrayList<Employee> getEmployeeArrayList(){
 
         return employeeArrayList;
     }
 
-    @RequestMapping("/update_emp")
+    @PutMapping("/update_emp")
     public String updateEmp(@RequestParam String name ,@RequestParam int  index){
         //get employee object
        Employee emp= employeeArrayList.get(index);//
@@ -37,7 +41,7 @@ public class EmpController {
         return "Record update_successfully...";
     }
 
-    @RequestMapping("remove_emp/{index}")
+    @DeleteMapping("remove_emp/{index}")
     public String removeEmp(@PathVariable int index){
         employeeArrayList.remove(index);
         return "record_deleted..";
