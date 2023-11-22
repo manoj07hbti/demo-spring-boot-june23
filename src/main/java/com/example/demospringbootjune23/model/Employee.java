8ad4a080ebb3a512f5fd3063c0a600a0345a3820
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-11-22 21:38:35 +0530

    DB Connection with Table creation

--- src/main/java/com/example/demospringbootjune23/model/Employee.java
+++ src/main/java/com/example/demospringbootjune23/model/Employee.java
@@ -1,10 +1,22 @@
 package com.example.demospringbootjune23.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="EMP_JUNE_23") // it will make this java class as Database Table
 public class Employee {
 
+    @Id // it will make this field primary key :it will allow only unique value to this column
+    @GeneratedValue // it will automatically provide value to id column
+    Long id;
     public String empName;
     public int    empId;
     public   String  dept;
+
+    //default constructor
+    public Employee() {
+    }
 
     public Employee(String empName, int empId, String dept) {
         this.empName = empName;
