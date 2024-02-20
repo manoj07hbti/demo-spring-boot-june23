51ba7218e5103a2fedbcaf73831fba88ca532d9c
jatin12345n <jitinshakya786jks@gmail.com>
2024-02-20 23:48:47 +0530

    UPDATE PATH VARIABLE

--- src/main/java/com/example/demospringbootjune23/controller/PathVariableController.java
+++ src/main/java/com/example/demospringbootjune23/controller/PathVariableController.java
@@ -9,22 +9,37 @@ import java.util.ArrayList;
 @RestController
 public class PathVariableController {
 
-    ArrayList<String> courses= new ArrayList<>();
+    ArrayList<String> courses = new ArrayList<>();
 
     //"endpoint_name/{path_variable_name}
     @RequestMapping("/message/{name}")
-    public String hello(@PathVariable String name){
-        return "Hello, Welcome to Spring boot..."+name;
+    public String hello(@PathVariable String name) {
+        return "Hello, Welcome to Spring boot..." + name;
     }
 
     @RequestMapping("/add/{a}/{b}")
-    public String add(@PathVariable int a, @PathVariable int b){
+    public String add(@PathVariable int a, @PathVariable int b) {
 
-        return "Addition of given input is :" +(a+b);
+        return "Addition of given input is :" + (a + b);
+    }
+
+    @RequestMapping("/sub/{a}/{b}")
+    public String sub(@PathVariable int a, @PathVariable int b) {
+        return "Subtraction of given input is :" + (a - b);
+    }
+
+    @RequestMapping("/multi/{a}/{b}")
+    public String mul(@PathVariable int a, @PathVariable int b) {
+        return "Multiplication of given input is :" + (a * b);
+    }
+
+    @RequestMapping("/div/{a}/{b}")
+    public String div(@PathVariable int a, @PathVariable int b) {
+        return "Division of given input is :" + (a / b);
     }
 
     @RequestMapping("/add_and_get_courses/{courseName}")
-    public ArrayList<String> addCourses(@PathVariable String courseName){
+    public ArrayList<String> addCourses(@PathVariable String courseName) {
 
 
         courses.add(courseName);
